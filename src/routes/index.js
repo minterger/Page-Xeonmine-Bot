ed403f671e98d2521f2acf8cfba557c16042b932
minterger <minterger@gmail.com>
2020-11-06 02:50:16 -0300

    update title for pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,15 @@ const app = express.Router();
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index.html', { index: 'active', comandos: '', sobremi: '' });
+    res.render('index.html', { title: 'Xeonmine', index: 'active', comandos: '', sobremi: '' });
   })
 
 app.get('/comandos', (req, res) => {
-    res.render('comandos.html', { index: '', comandos: 'active', sobremi: '' });
+    res.render('comandos.html', { title: 'Xeonmine - Comandos', index: '', comandos: 'active', sobremi: '' });
   })
 
 app.get('/sobremi', (req, res) => {
-    res.render('sobremi.html', { index: '', comandos: '', sobremi: 'active' });
+    res.render('sobremi.html', { title: 'Xeonmine - Sobre Mi', index: '', comandos: '', sobremi: 'active' });
   })
 
 module.exports = app;
