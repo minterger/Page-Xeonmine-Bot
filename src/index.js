286246e181bffa421cc7b3278a30776741eab174
minterger <minterger@gmail.com>
2020-11-05 21:44:26 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 //settings
 const port = 3000;
 
 
 //routes
-
+app.use('/', routes)
 
 //listen port
 app.listen(port, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
+const path = require('path')
 const express = require('express');
+const app = express.Router();
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-  });
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+  })
 
 app.get('/app', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/account.html'));
-  });
+    res.sendFile(path.join(__dirname, '../views/account.html'));
+  })
+
+module.exports = app;
