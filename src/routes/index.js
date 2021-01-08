a0cb41344bf1e706cc2fc09c09491f49257dc149
minterger <minterger@gmail.com>
2021-01-07 21:11:49 -0300

    logo normal

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,15 @@ const app = express.Router()
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index.html', { img: '/img/bokeh.jpg', logo: 'img/logonavidad.jpeg', title: 'Xeonmine', index: 'active', comandos: '', sobremi: '' })
+    res.render('index.html', { img: '/img/bokeh.jpg', logo: 'img/logo.jpeg', title: 'Xeonmine', index: 'active', comandos: '', sobremi: '' })
   })
 
 app.get('/comandos', (req, res) => {
-    res.render('comandos.html', { img: '/img/bokeh.jpg', logo: 'img/logonavidad.jpeg', title: 'Xeonmine - Comandos', index: '', comandos: 'active', sobremi: '' })
+    res.render('comandos.html', { img: '/img/bokeh.jpg', logo: 'img/logo.jpeg', title: 'Xeonmine - Comandos', index: '', comandos: 'active', sobremi: '' })
   })
 
 app.get('/sobremi', (req, res) => {
-    res.render('sobremi.html', { img: '/img/bokeh.jpg', logo: 'img/logonavidad.jpeg', title: 'Xeonmine - Sobre Mi', index: '', comandos: '', sobremi: 'active' })
+    res.render('sobremi.html', { img: '/img/bokeh.jpg', logo: 'img/logo.jpeg', title: 'Xeonmine - Sobre Mi', index: '', comandos: '', sobremi: 'active' })
   })
 
 module.exports = app
